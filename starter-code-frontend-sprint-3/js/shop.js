f4dabf1217572fe630d907851030f936fbeb79a5
toniprats07 <119038749+toniprats07@users.noreply.github.com>
2023-05-18 09:39:30 +0200

    Ejercicio 6

--- starter-code-frontend-sprint-3/js/shop.js
+++ starter-code-frontend-sprint-3/js/shop.js
@@ -71,7 +71,8 @@ var cart = [];
 
 var total = 0;
 
-var subtotalWithDiscount = 0;
+var subtotalWithDiscount1 = 0;
+var subtotalWithDiscount2 = 0
 
 // Exercise 1
 function buy(id) {
@@ -87,8 +88,12 @@ function buy(id) {
     
 // Exercise 2
 function cleanCart() {
-    cartList.length = 0;
-    console.log(cartList);
+    cart.length = 0;
+    var DOMcleanCart = document.getElementById('cart_list');
+    var DOMcleanTotal = document.getElementById('total_price');
+    DOMcleanCart.textContent = ' ';
+    DOMcleanTotal.textContent = ' ';
+    console.log(cart);
 }
 
 // Exercise 3
@@ -129,38 +134,101 @@ function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
     for (let i = 0; i < cart.length; i++){
         if (cart[i].name === 'cooking oil' && cart[i].quantity >= 3){
-            subtotalWithDiscount = cart[i].quantity * 10;
-            console.log('total a pagar aceite ' +subtotalWithDiscount);
+            subtotalWithDiscount1 = cart[i].quantity * 10;
+            console.log('total a pagar aceite ' +subtotalWithDiscount1);
+            total += subtotalWithDiscount1;   
         }else if (cart[i].name === 'Instant cupcake mixture' && cart[i].quantity >= 10) {
-            subtotalWithDiscount = cart[i].quantity * cart[i].price * 2/3;
-            console.log('total a pagar pastel ' +subtotalWithDiscount);
+            subtotalWithDiscount2 = cart[i].quantity * cart[i].price * 2/3;
+            console.log('total a pagar pastel ' +subtotalWithDiscount2);
+            total += subtotalWithDiscount2;
+        } else {
+           total += cart[i].quantity * cart[i].price;
         }
+        
     }
 }
 
 // Exercise 6
-function printCart() {
+    function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    let tbody = document.getElementById('cart_list')
+    for (let i = 0; i < cart.length; i++){
+        //nombre
+        let tr = document.createElement("tr");
+        let tdNombre = document.createElement("td");
+        tdNombre.textContent = cart[i].name;
+        tr.appendChild(tdNombre);
+        //precio
+        let tdPrecio = document.createElement("td");
+        tdPrecio.textContent = cart[i].price +' €';
+        tr.appendChild(tdPrecio);
+        //cantidad
+        let tdCantidad = document.createElement("td");
+        tdCantidad.textContent = cart[i].quantity;
+        tr.appendChild(tdCantidad);
+        //total
+        if (cart[i].name === 'cooking oil'){
+            let tdTotal = document.createElement("td");
+            tdTotal.textContent = subtotalWithDiscount1.toFixed(2) +' €';
+            tr.appendChild(tdTotal);
+        } else if (cart[i].name === 'Instant cupcake mixture'){
+            let tdTotal = document.createElement("td");
+            tdTotal.textContent = subtotalWithDiscount2.toFixed(2) +' €';
+            tr.appendChild(tdTotal);
+
+        } else {
+            let tdTotal = document.createElement("td");
+            tdTotal.textContent = (cart[i].quantity*cart[i].price).toFixed(2) +' €';
+            tr.appendChild(tdTotal);
+        }
+
+        //agragar a la tabla
+        tbody.appendChild(tr);
+
+        // Botones de incremento y decremento
+        let tdButtons = document.createElement('td');
+        //boton decremento
+        let decrementButton = document.createElement('button');
+        decrementButton.classList.add('btn', 'btn-danger');
+        decrementButton.textContent = '-';
+        decrementButton.addEventListener('click', () => removeFromCart(i, true));  
+        //boton incremento
+        let incrementButton = document.createElement('button');
+        incrementButton.classList.add('btn', 'btn-success');
+        incrementButton.textContent = '+';
+        incrementButton.addEventListener('click', () => removeFromCart(i, false));
+        //agregar botones
+        tdButtons.appendChild(decrementButton);
+        tdButtons.appendChild(incrementButton);
+        tr.appendChild(tdButtons);
+    }
+    let totalPrice = document.getElementById('total_price');
+    totalPrice.textContent = total.toFixed(2) +' €';
 }
 
 
 // ** Nivell II **
 
-// Exercise 7
+// Exercise 8
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    
 }
 
-// Exercise 8
+// Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    
 }
+
 
 function open_modal(){
 	console.log("Open Modal");
-    calculateTotal();
+    generateCart()
+    applyPromotionsCart();
 	printCart();
 }
+ 
